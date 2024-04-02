f136fe7e315c153e2b29b9174adbeb585c4922fe
Usuario <Usuario@CPD03-M14>
2024-04-02 11:18:14 -0300

    Adicionar arquivos de projeto.

---
+++ MatriculaAlunos/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatriculaAlunos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int desenvolvimento = 0;
+            int administracao = 0;
+            int mecatronica = 0;
+            int edificacoes = 0;
+
+            //Loop ->
+            bool continuarMatricula = true; // "bool" -> só le verdadeiro ou falso
+            while (continuarMatricula)
+            {
+                // opções ->
+                Console.WriteLine("Escolha o curso desejado:");
+                Console.WriteLine("D - Desenvolvimento");
+                Console.WriteLine("A - Administração");
+                Console.WriteLine("M - Mecatrônica");
+                Console.WriteLine("E - Edificações");
+
+                char opcao = char.ToUpper(Console.ReadKey().KeyChar); //"char.ToUpper" faz pegar o minúsculo
+                Console.WriteLine(); // -> pula linha
+
+                // respsota ->
+                switch (opcao)
+                {
+                    case 'D': // por algum motivo está funcionando com minúscula
+                        if (desenvolvimento < 5)
+                        {
+                            desenvolvimento++;
+                            Console.WriteLine("Matrícula realizada em Desenvolvimento.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Desculpe, não há vagas disponíveis para Desenvolvimento.");
+                        }
+                        break;
+                    case 'A':
+                        if (administracao < 5)
+                        {
+                            administracao++;
+                            Console.WriteLine("Matrícula realizada em Administração.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Desculpe, não há vagas disponíveis para Administração.");
+                        }
+                        break;
+                    case 'M':
+                        if (mecatronica < 5)
+                        {
+                            mecatronica++;
+                            Console.WriteLine("Matrícula realizada em Mecatrônica.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Desculpe, não há vagas disponíveis para Mecatrônica.");
+                        }
+                        break;
+                    case 'E':
+                        if (edificacoes < 5)
+                        {
+                            edificacoes++;
+                            Console.WriteLine("Matrícula realizada em Edificações.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Desculpe, não há vagas disponíveis para Edificações.");
+                        }
+                        break;
+                    default: //se nada der certo ->
+                        Console.WriteLine("Opção inválida.");
+                        break;
+                }
+
+                // Pergunta se continuará ou não ->
+                Console.WriteLine("Deseja matricular mais um aluno? (S/N)");
+                char continuar = char.ToUpper(Console.ReadKey().KeyChar);
+                Console.WriteLine(); // Avança para uma nova linha após a entrada do usuário
+
+                // verificação ->
+                if (continuar != 'S') //"!=" -> "Diferente de..."
+                {
+                    continuarMatricula = false;
+                }
+            }
+
+            // Exibir tudo ->
+            Console.WriteLine("Total de matrículas por curso:");
+            Console.WriteLine("Desenvolvimento: " + desenvolvimento);
+            Console.WriteLine("Administração: " + administracao);
+            Console.WriteLine("Mecatrônica: " + mecatronica);
+            Console.WriteLine("Edificações: " + edificacoes);
+
+
+            //Final ->
+            Console.WriteLine("Pressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MatriculaAlunos/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("MatriculaAlunos")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MatriculaAlunos")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("509ba28b-330c-4530-a4c8-a7b3f5b5f931")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
